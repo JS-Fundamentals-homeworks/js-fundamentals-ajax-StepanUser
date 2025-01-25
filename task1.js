30ba88f121aae1a4e9d43e5c3f0ecd21cc724249
Stepan Seniak <stepanseniak@gmail.com>
2025-01-25 11:31:21 +0200

    feat: Complete `js fundamentals ajax` tasks.

--- task1.js
+++ task1.js
@@ -1,7 +1,29 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
+
+const userList = document.querySelector(".usersList");
+
+(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    if (!response.ok) {
+      console.log(`HTTP error! Status: ${response.status}`);
+    }
+
+    const users = await response.json();
+
+    users.forEach((user) => {
+      const li = document.createElement("li");
+      li.textContent = user.name;
+      userList.appendChild(li);
+    });
+  } catch (error) {
+    console.error("Error during loading user names:", error);
+  }
+})();
--- task2.js
+++ task2.js
@@ -5,3 +5,24 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const getUserButton = document.querySelector("#getUserButton");
+const userNameInput = document.querySelector("#userNameInput");
+const userCityElement = document.querySelector("#userCity");
+
+getUserButton.addEventListener("click", async () => {
+  const userName = userNameInput.value;
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    const user = users.find((user) => user.name === userName);
+    if (user) {
+      const userCity = user.address.city;
+      userCityElement.textContent = userCity;
+    } else {
+      console.log("User not found");
+    }
+  } catch (error) {
+    console.error("Error during loading user names:", error);
+  }
+});
